72026949cb5884e23039001996fcf05d7b1e371c
fariz <farizwhyzn@gmail.com>
2024-04-24 00:52:54 +0700

    Update homepage to use client's session

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+'use client'
+
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import User from './components/User';
+import {signOut, useSession} from "next-auth/react";
 
-export default async function Home() {
-    const session = await getServerSession(authOptions)
+export default function Home() {
+    const { data:session } = useSession();
   return (
       <section>
         <h1>Home</h1>
         <p>Welcome to the home page.</p>
-          <pre>{JSON.stringify(session)}</pre>
+          <p>Hi {session?.user?.email}</p>
+          <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000/login' })}>Sign Out</button>
             <User />
       </section>
   );
