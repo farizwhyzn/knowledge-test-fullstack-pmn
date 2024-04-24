24d8ff97067a12ba966525bea374a5de11ea29b9
fariz <farizwhyzn@gmail.com>
2024-04-24 16:58:56 +0700

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,82 @@
 'use client'
 
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 import User from './components/User';
 import {signOut, useSession} from "next-auth/react";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import axios from "axios";
+import {useEffect, useState} from "react";
+import { UserNav } from "@/components/user-nav"
 
 export default function Home() {
-    const { data:session } = useSession();
+    const { data:session, status } = useSession();
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/users')
+            .then((response) => {
+                setUsers(response.data)
+            })
+    }, [])
+
+    if (status !== 'authenticated') {
+        return (
+            <div>
+                <h1>Access Denied</h1>
+                <p>You must be signed in to view this page.</p>
+            </div>
+        )
+    }
+
   return (
-      <section>
-        <h1>Home</h1>
-        <p>Welcome to the home page.</p>
-          <p>Hi {session?.user?.email}</p>
-          <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000/login' })}>Sign Out</button>
-            <User />
-      </section>
+      <>
+          <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+              <div className="flex items-center justify-between space-y-2">
+                  <div>
+                      <h2 className="text-2xl font-bold tracking-tight">Welcome back,!</h2>
+                      <p className="text-muted-foreground">
+                          Here&apos;s a list of users for you!
+                      </p>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                      <UserNav />
+                  </div>
+              </div>
+              <Table>
+                  <TableHeader>
+                      <TableRow>
+                          <TableHead className="w-[100px]">No.</TableHead>
+                          <TableHead>Name</TableHead>
+                          <TableHead>Email</TableHead>
+                          <TableHead>Role</TableHead>
+                      </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                      {users.map((user, index) => (
+                          <TableRow key={user.id}>
+                              <TableCell className="font-medium">{ index+1 }</TableCell>
+                              <TableCell>{user.name}</TableCell>
+                              <TableCell>{user.email}</TableCell>
+                              <TableCell>{user.role}</TableCell>
+                          </TableRow>
+                      ))}
+                  </TableBody>
+                  <TableFooter>
+                      <TableRow>
+                          <TableCell colSpan={3}>Total of {users.length} user(s) rendered</TableCell>
+                      </TableRow>
+                  </TableFooter>
+              </Table>
+          </div>
+      </>
   );
 }
---
+++ components/user-nav.tsx
@@ -0,0 +1,63 @@
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {signOut, useSession} from "next-auth/react";
+
+export function UserNav() {
+    const { data:session, status } = useSession();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback>FWD</AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium leading-none">{session?.user?.name}</p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                            {session?.user?.email}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        Profile
+                        <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Home
+                        <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>Products</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000/login' })}>
+                    <DropdownMenuItem>
+                        Log out
+                        <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </button>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
